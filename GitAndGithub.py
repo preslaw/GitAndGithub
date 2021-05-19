dea5ba7764c3583cf307a2db4f74107cda6db1f2
Marcin Biszczak <preslaw@wp.pl>
2021-05-19 15:08:40 +0200

    druga zmiana na trzeciej

--- GitAndGithub.py
+++ GitAndGithub.py
@@ -17,3 +17,4 @@ print('A teraz dopisuję na gałęzi druga')
 
 print ('to działa dobrze. łącze z mainem!')
 #teraz dopiero przesiadłem sie na trzecia galaz
+# na trzeciej galezi siedzi się wygodnie wiecej lacze ja z main
